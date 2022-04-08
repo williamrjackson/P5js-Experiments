b57fb37a8ad3e09434b2563811e27468a7c99caa
William Jackson <wjackson@mos.org>
2022-04-07 21:14:30 -0400

    Restore base step 1

--- TruchetTiles/sketch.js
+++ TruchetTiles/sketch.js
@@ -1,14 +1,36 @@
-
+var cellSize = 40;
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    background(30);  
     angleMode(DEGREES);
+    background(30);  
+    stroke(150)
+    strokeWeight(3)
+    noFill();
+    
+    for (var x = 0; x < width; x+=cellSize){
+        for (var y = 0; y < height; y+=cellSize){
+            drawCell(x,y, random() < 0.5)
+        }
+    }
+    drawCell(40, 40, true);
 }
 
 function draw() {
-    fill(0, 0, 139)
-    var squareSize = 100;
-    rect(width / 2 - squareSize / 2, 
-        height / 2 - squareSize / 2, 
-        squareSize);
+}
+
+function drawCell(x, y, flip)
+{
+    push();
+    translate(x, y);
+    if (flip)
+    {
+        arc(0, 0, cellSize, cellSize, 0, 90);
+        arc(cellSize, cellSize, cellSize, cellSize, 180, 270);
+    }
+    else
+    {
+        arc(cellSize, 0, cellSize, cellSize, 90, 180);
+        arc(0, cellSize, cellSize, cellSize, 270, 360);
+    }
+    pop();
 }
